9bcc34e58b182a6358900adb1398d0802c4ca8e8
Oguzhan YAGCI <oyagci@student.42.fr>
2019-08-08 08:48:51 +0200

    Change log ouput

--- accept_new_clients.c
+++ accept_new_clients.c
@@ -16,9 +16,9 @@ int		accept_new_clients(struct s_server *server)
 
 	cli_len = sizeof(cli_addr);
 	if (FD_ISSET(server->sockfd, &server->readfds)) {
-		confd = accept(sockfd, (struct sockaddr *)&cli_addr, &cli_len);
+		confd = accept(server->sockfd, (struct sockaddr *)&cli_addr, &cli_len);
 
-		LOG(LOGDEBUG, "%s: connected", inet_ntoa(cli_addr.sin_addr));
+		LOG(LOGDEBUG, "%s: connected on fd %d", inet_ntoa(cli_addr.sin_addr), confd);
 
 		// Add connection to the clients list
 		client = ft_memalloc(sizeof(*client));
@@ -26,7 +26,6 @@ int		accept_new_clients(struct s_server *server)
 		elem = ft_lstnew(NULL, 0);
 		elem->content = client;
 		ft_lstadd(&server->clients, elem);
-		LOG(LOGDEBUG, "New connection on fd %d", confd);
 	}
 	return (0);
 }
