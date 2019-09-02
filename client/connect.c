965b675c69b267352b9c1829926ef93192889a99
Oguzhan YAGCI <oyagci@student.42.fr>
2019-09-02 10:15:37 +0200

    Refactor client.connect

--- client/connect.c
+++ client/connect.c
@@ -8,44 +8,46 @@
 #include <stdlib.h>
 #include <errno.h>
 
-int		client_connect(struct s_client *const self, 
-					   struct s_client_msg const *const cmd)
+static int	do_conn(struct s_client *self, char const *addr, char const *portstr,
+	int portno)
 {
-	int					portno;
 	struct addrinfo		*server = NULL;
 	struct addrinfo		hints;
+	int					ret;
+	int					gaierr;
 
-	if (self->servsock > 0)
-	{
-		LOG(LOGWARN, "Already connected");
-		return (-1);
-	}
-	portno = ft_atoi(cmd->params[1]);
-
-	LOG(LOGDEBUG, "Opening connection to %s:%d", cmd->params[0], portno);
-
+	ret = -1;
+	printf(" - Connecting to %s:%d\n", addr, portno);
 	ft_bzero(&hints, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
-
-	int ret = 0;
-	if ((ret = getaddrinfo(cmd->params[0], cmd->params[1], &hints, &server)) != 0)
-	{
-		LOG(LOGERR, "getaddrinfo: %s", gai_strerror(ret));
-		exit(1);
-		return (0);
-	}
-	if (-1 == (self->servsock = socket(server->ai_family, server->ai_socktype,
+	if ((gaierr = getaddrinfo(addr, portstr, &hints, &server)) != 0)
+		LOG(LOGERR, "getaddrinfo: %s", gai_strerror(gaierr));
+	else if (-1 == (self->servsock = socket(server->ai_family,
+				server->ai_socktype,
 				server->ai_protocol)))
-	{
 		LOG(LOGERR, "Could not create socket (%s)", strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-	if (connect(self->servsock, server->ai_addr, server->ai_addrlen) < 0)
-	{
-		LOG(LOGERR, "Could not connect\n");
-		printf(" * Could not connect to %s:%d (%s)\n", cmd->params[0], portno,
+	else if (connect(self->servsock, server->ai_addr, server->ai_addrlen) < 0)
+		printf(" - Could not connect to %s:%d (%s)\n", addr, portno,
 				strerror(errno));
+	else
+	{
+		printf(" - Connected to %s\n", addr);
+		ret = 0;
 	}
-	return (0);
+	return (ret);
+}
+
+int			client_connect(struct s_client *const self, 
+					   struct s_client_msg const *const cmd)
+{
+	unsigned short	portno;
+
+	if (self->servsock > 0)
+	{
+		printf(" - Sorry, already connected\n");
+		return (-1);
+	}
+	portno = ft_atoi(cmd->params[1]);
+	return (do_conn(self, cmd->params[0], cmd->params[1], portno));
 }
