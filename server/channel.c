fbea063536e8bd371adbc9acdb6c48f2d5360346
Oguzhan YAGCI <oyagci@student.42.fr>
2019-09-07 19:46:29 +0200

    Fix being able to join multiple times a channel

--- server/channel.c
+++ server/channel.c
@@ -1,19 +1,52 @@
 #include "libft.h"
 #include "server.h"
 #include "logger.h"
+#include <stdlib.h>
+
+int		client_in_channel(struct s_client const *const client,
+		struct s_channel const *const channel)
+{
+	t_list			*elem;
+	struct s_client	*c;
+	int				ret;
+
+	ret = 0;
+	elem = channel->clients;
+	while (elem)
+	{
+		c = elem->content;
+		if (ft_strnequ(client->nickname, c->nickname, NICK_SIZE))
+		{
+			ret = -1;
+			break ;
+		}
+		elem = elem->next;
+	}
+	return (ret);
+}
 
 int		channel_add_client(struct s_channel *channel, struct s_client *client)
 {
 	t_list	*elem;
+	int		ret;
 
+	ret = 0;
 	elem = ft_lstnew(0, 0);
 	if (!elem)
 		return (-1);
 	elem->content = client;
-	ft_lstpush(&channel->clients, elem);
-	LOG(LOGDEBUG, "Client %s added to channel %.50s", client->nickname,
-		channel->name);
-	return (0);
+	if (!client_in_channel(client, channel))
+	{
+		ft_lstpush(&channel->clients, elem);
+		LOG(LOGDEBUG, "Client %s added to channel %.50s", client->nickname,
+			channel->name);
+	}
+	else
+	{
+		free(elem);
+		ret = -1;
+	}
+	return (ret);
 }
 
 int		channel_rm_nick(struct s_channel *const channel, char const *const nick)
