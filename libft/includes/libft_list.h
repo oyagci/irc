c63115a9b8bc6337fb4c46816d5d1340a7e24d07
Oguzhan YAGCI <oyagci@student.42.fr>
2019-09-09 13:10:41 +0200

    Norme libft_list.h

--- libft/includes/libft_list.h
+++ libft/includes/libft_list.h
@@ -15,37 +15,39 @@
 
 # include <string.h>
 
-typedef struct		s_list
+typedef struct			s_list
 {
-	void			*content;
-	size_t			content_size;
-	struct s_list	*next;
-}					t_list;
+	void				*content;
+	size_t				content_size;
+	struct s_list		*next;
+}						t_list;
 
 typedef	struct s_dlist	t_dlist;
 struct					s_dlist
 {
-	void		*content;
-	size_t		content_size;
-	t_dlist		*prev;
-	t_dlist		*next;
+	void				*content;
+	size_t				content_size;
+	t_dlist				*prev;
+	t_dlist				*next;
 };
 
-t_list				*ft_lstnew(void const *content, size_t content_size);
-void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
-void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
-void				ft_lstadd(t_list **alst, t_list *new);
-void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
-t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
-t_list				*ft_lstpush_filter(
-						t_list *lst,
-						void *content, size_t content_size,
-						int (*f)(t_list *, void *, size_t));
-void				ft_lstpush(t_list **list, t_list *elem);
+t_list					*ft_lstnew(void const *content, size_t content_size);
+void					ft_lstdelone(t_list **alst,
+							void (*del)(void *, size_t));
+void					ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void					ft_lstadd(t_list **alst, t_list *new);
+void					ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list					*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+t_list					*ft_lstpush_filter(
+							t_list *lst,
+							void *content, size_t content_size,
+							int (*f)(t_list *, void *, size_t));
+void					ft_lstpush(t_list **list, t_list *elem);
 
-t_dlist				*ft_dlstnew(void const *content, size_t content_size);
-void				ft_dlstdelone(t_dlist **alst, void (*del)(void *, size_t));
-void				ft_dlstdel(t_dlist **alst, void (*del)(void *, size_t));
-void				ft_dlstadd(t_dlist **alst, t_dlist *elem);
+t_dlist					*ft_dlstnew(void const *content, size_t content_size);
+void					ft_dlstdelone(t_dlist **alst,
+							void (*del)(void *, size_t));
+void					ft_dlstdel(t_dlist **alst, void (*del)(void *, size_t));
+void					ft_dlstadd(t_dlist **alst, t_dlist *elem);
 
 #endif
