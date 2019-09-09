39c9f993f9816423225d2a6c5448f9bbac6a1557
Oguzhan YAGCI <oyagci@student.42.fr>
2019-09-09 12:57:05 +0200

    Norme parse_input.c

--- client/parse_input.c
+++ client/parse_input.c
@@ -4,17 +4,13 @@
 #include "libft.h"
 #include <stdlib.h>
 
-enum e_client_cmd	set_command(char const **inputp, struct s_client_msg *buf)
+void				set_command(char const **inputp, struct s_client_msg *buf)
 {
 	const struct s_client_cmds	cmds[] = {
-		{ .str = "connect", .cmd = CMD_CONNECT, .nparam = 2 },
-		{ .str = "nick", .cmd = CMD_NICK, .nparam = 1 },
-		{ .str = "join", .cmd = CMD_JOIN, .nparam = 1 },
-		{ .str = "leave", .cmd = CMD_LEAVE, .nparam = 1 },
-		{ .str = "msg", .cmd = CMD_MSG, .nparam = 2 },
-		{ .str = "quit", .cmd = CMD_QUIT, .nparam = 1 },
-		{ .str = "who", .cmd = CMD_WHO, .nparam = 0 },
-		{ .str = "disconnect", .cmd = CMD_QUIT, .nparam = 0 }
+		{ "nick", CMD_NICK, 1 }, { "join", CMD_JOIN, 1 },
+		{ "leave", CMD_LEAVE, 1 }, { "msg", CMD_MSG, 2 },
+		{ "quit", CMD_QUIT, 1 }, { "who", CMD_WHO, 0 },
+		{ "connect", CMD_CONNECT, 2 }, { "disconnect", CMD_QUIT, 0 }
 	};
 	char const					*input;
 	int							ii;
@@ -32,11 +28,19 @@ enum e_client_cmd	set_command(char const **inputp, struct s_client_msg *buf)
 			*inputp += ii;
 			buf->cmd = cmds[jj].cmd;
 			buf->nparam = cmds[jj].nparam;
-			return (cmds[jj].cmd);
 		}
 		jj += 1;
 	}
-	return (CMD_NONE);
+}
+
+void				set_param(char *buf, char const *input,
+	size_t start, size_t end)
+{
+	char	tmp[255];
+
+	ft_memset(tmp, 0, sizeof(tmp));
+	ft_strlcpy(tmp, input + start, end - start);
+	ft_strlcpy(buf, tmp, 255);
 }
 
 size_t				set_params(char const *input, struct s_client_msg *buf)
@@ -44,10 +48,7 @@ size_t				set_params(char const *input, struct s_client_msg *buf)
 	int		start;
 	int		end;
 	size_t	ii;
-	char	tmp[255];
 
-	if (*input == 0)
-		return (0);
 	start = 0;
 	end = 0;
 	ii = 0;
@@ -58,20 +59,30 @@ size_t				set_params(char const *input, struct s_client_msg *buf)
 			end = start;
 			while (input[end] != ' ' && input[end] != '\0')
 				end++;
-			ft_memset(tmp, 0, sizeof(tmp));
-			ft_strncpy(tmp, input + start, end - start);
-			ft_strlcpy(buf->params[ii], tmp, 255);
+			set_param(buf->params[ii], input, start, end);
 			if (input[end] == ' ')
 				end++;
 			start = end;
 			ii += 1;
 		}
-		if (input[start] != 0) {
-			ft_strlcpy(buf->params[ii], input + start, 255);
-			ii += 1;
-		}
+		if (input[start] != 0)
+			ft_strlcpy(buf->params[ii++], input + start, 255);
 	}
 	return (ii);
+}
+
+void				parse_cmd(struct s_client_msg **buf, char const *input)
+{
+	input += 1;
+	set_command(&input, *buf);
+	if (*input == ' ')
+		input++;
+	if (set_params(input, *buf) < (*buf)->nparam)
+	{
+		printf("Not enough parameters given (expected %ld)\n", (*buf)->nparam);
+		free(*buf);
+		*buf = NULL;
+	}
 }
 
 struct s_client_msg	*parse_input(struct s_client *const self, char const *input)
@@ -83,18 +94,7 @@ struct s_client_msg	*parse_input(struct s_client *const self, char const *input)
 		exit(EXIT_FAILURE);
 	msg->cmd = CMD_NONE;
 	if (input[0] == '/')
-	{
-		input += 1;
-		set_command(&input, msg);
-		if (*input == ' ')
-			input++;
-		if (set_params(input, msg) < msg->nparam)
-		{
-			printf("Not enough parameters given (expected %ld)\n", msg->nparam);
-			free(msg);
-			msg = NULL;
-		}
-	}
+		parse_cmd(&msg, input);
 	else if (self->channel)
 	{
 		msg->cmd = CMD_MSG;
