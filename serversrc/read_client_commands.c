40a029424260cf9c7d2e298712587b8784d35191
Oguzhan YAGCI <oyagci@student.42.fr>
2019-09-18 11:26:28 +0200

    Add better error handling in read_clients_command

--- serversrc/read_client_commands.c
+++ serversrc/read_client_commands.c
@@ -52,26 +52,39 @@ static ssize_t read_to_buffer(int cfd, t_cbuf_handle cbuf)
 	return (complete);
 }
 
-static void	do_command(struct s_server *self, struct s_client *client)
+static int do_command(struct s_server *self, struct s_client *client)
 {
-	char			buf[512];
-	unsigned char	data;
-	int				ii;
+	char buf[512] = { 0 };
+	unsigned char data = 0;
+	int ii = 0;
 
 	ft_memset(buf, 0, 512);
 	ii = 0;
 	while (ii < 512)
 	{
-		if (cbuf_get(client->cbuf, &data) < 0)
+		if (cbuf_get(client->cbuf, &data) < 0) {
+			/* No more data, command is complete */
 			break ;
+		}
+
 		buf[ii] = data;
-		if (buf[ii] == '\n')
+
+		if (buf[ii] == '\n') {
+			/* A newline means command is complete */
 			break ;
+		}
 		ii += 1;
 	}
-	self->exec_cmd(self, client, buf);
+	return (self->exec_cmd(self, client, buf));
 }
 
+/*
+ * Read and execute commands read from client input
+ * Returns -1 on error or the number of commands read.
+ *
+ * FIXME: This function should only READ commands and put them inside a buffer,
+ *        execution should be done elsewhere
+ */
 int			read_clients_command(struct s_server *const self)
 {
 	t_list			*next;
@@ -90,9 +103,14 @@ int			read_clients_command(struct s_server *const self)
 			ret = -1;
 			break ;
 		}
+		ret = complete;
 		while (complete > 0) {
+			if (do_command(self, client) < 0) {
+				fprintf(stderr, "%s:%d: Could not execute command\n",
+					__FUNCTION__, __LINE__);
+				/* TODO: Handle error */
+			}
 			complete -= 1;
-			do_command(self, client);
 		}
 		cur = next;
 	}
