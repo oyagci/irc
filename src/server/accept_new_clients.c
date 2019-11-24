4fd751d0814afe3a6993f26f466dedfd4aea8254
Oguzhan YAGCI <oyagci@student.42.fr>
2019-11-24 14:13:39 +0100

    Fix bad new client initialization causing invalid writes/reads

--- src/server/accept_new_clients.c
+++ src/server/accept_new_clients.c
@@ -28,7 +28,7 @@ static int		setnosigpipe(void)
 	return (0);
 }
 
-static int		add_client(struct s_server *self, t_client *c)
+static int		get_client_slot(t_server *self)
 {
 	size_t	i;
 
@@ -36,13 +36,25 @@ static int		add_client(struct s_server *self, t_client *c)
 	while (i < NCLIENTS)
 	{
 		if (self->clients[i].fd == 0)
-		{
-			ft_memcpy(self->clients + i, c, sizeof(t_client));
-			return (0);
-		}
+			return (i);
 		i += 1;
 	}
 	return (-1);
+}
+
+static int		add_new_client(t_server *server, int fd)
+{
+	int			slot;
+	t_client	*client;
+	
+	slot = get_client_slot(server);
+	if (slot < 0)
+		return (-1);
+	client = server->clients + slot;
+	client->fd = fd;
+	client->server = server;
+	client->cbuf = cbuf_init(client->raw_buffer, sizeof(client->raw_buffer));
+	return (0);
 }
 
 void			client_init(t_client *client, struct s_server *s, int fd)
@@ -72,7 +84,7 @@ int				accept_new_clients(struct s_server *server)
 	}
 	setnosigpipe();
 	client_init(&client, server, confd);
-	if (add_client(server, &client) < 0)
+	if (add_new_client(server, confd) < 0)
 	{
 		ERROR("Could not add client to list. Closing connection...");
 		close(confd);
