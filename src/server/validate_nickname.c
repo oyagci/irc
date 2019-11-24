bd3bb06966191576c551a84b49728c6eb8da94c3
Oguzhan YAGCI <oyagci@student.42.fr>
2019-11-24 14:40:39 +0100

    Add missing file

---
+++ src/server/validate_nickname.c
@@ -0,0 +1,27 @@
+#include "parser.h"
+
+static int	is_valid_char(char c)
+{
+	return (letter(c) || special(c) || digit(c) || c == '-');
+}
+
+int			validate_nickname(char const *input, char **buf)
+{
+	int	ii;
+
+	ii = 0;
+	if (letter(*input) || special(*input))
+	{
+		ii += 1;
+		while (input[ii] && is_valid_char(input[ii]))
+			ii += 1;
+		if (input[ii] != ' ' && input[ii] != '\0')
+			return (0);
+	}
+	if (ii < 9 && ii > 0)
+	{
+		*buf = ft_strndup(input, ii);
+		return (ii);
+	}
+	return (0);
+}
